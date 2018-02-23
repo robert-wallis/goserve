c95a59709628bd71b74f7662ac277a4e4889ebfc
Robert Wallis <smilingrob@gmail.com>
2018-02-23 00:38:14 -0800

    basic static http server

---
+++ goserve.go
@@ -0,0 +1,20 @@
+// Copyright (C) 2018 Robert A. Wallis, All Rights Reserved
+package main
+
+import (
+	"flag"
+	"log"
+	"net/http"
+)
+
+var port = flag.String("port", "localhost:8000", "server binding")
+
+func main() {
+	flag.Parse()
+	var fileServer = http.FileServer(http.Dir("."))
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		log.Println(r.Method, r.RequestURI, r.RemoteAddr)
+		fileServer.ServeHTTP(w, r)
+	})
+	log.Fatal(http.ListenAndServe(*port, nil))
+}
